777a42c471d16260596074e22131e8c267a02626
thomasloesch <tommy.loesch@gmail.com>
2016-12-17 22:31:39 -0500

    Added solution to day 5 part 2

--- Day5/day5b.py
+++ Day5/day5b.py
@@ -11,7 +11,7 @@ def CheckForFiveZeroes( s ):
 	return True
 
 def BetweenZeroAndSeven( c ):
-	return ord(c) in range(ord('0'), ord('7'))
+	return ord(c) in range(ord('0'), ord('8'))
 
 out = ""
 
@@ -20,11 +20,22 @@ passDict = { '0': None, '1': None, '2': None, '3': None, '4': None, '5': None, '
 j = 0
 while len(out) < 8:
 	m = hashlib.md5()
-	m.update(INPUT + str(j))
+	m.update((INPUT + str(j)).encode('utf-8'))
 
 	s = m.hexdigest()
 	#print("{}{}: {}".format(INPUT, j, s))
-	if(CheckForFiveZeroes(s))
-		out += s[5]
-		print("{}: {}".format(len(out), s[5]))
+	if(CheckForFiveZeroes(s)):
+		msg = "ERROR"
+		if(BetweenZeroAndSeven(s[5])):
+			if(passDict[s[5]] == None):
+				passDict[s[5]] = s[6]
+				msg = "Added"
+				out += s[6]
+			else:
+				msg = "Already found, skipping"
+		else:
+			msg = "Not a valid position, skipping"
+		print("{}\t{}: {};\t".format(j, s[5], s[6]) + msg)
 	j += 1
+
+print(passDict['0'] + passDict['1'] + passDict['2'] + passDict['3'] + passDict['4'] + passDict['5'] + passDict['6'] + passDict['7'])
